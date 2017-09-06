fbd767859d65170b51f0796f29e800855de79a46
pipeline <pipeline@syncfusion.com>
2017-09-06 11:49:46 +0000

    config(EJ2-3882): release changes development to master

--- src/common/plnk-template/main.ts
+++ src/common/plnk-template/main.ts
@@ -1,6 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { AppModule } from './app.module';
+import { enableRipple } from '@syncfusion/ej2-base';
+
+enableRipple(true);
 
 enableProdMode();
 platformBrowserDynamic().bootstrapModule(AppModule);
