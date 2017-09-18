61fa941f3b1b88a5da210f7eda47ba8f07eb0a52
pipeline <pipeline@syncfusion.com>
2017-09-18 15:16:54 +0000

    sample(ej2-1920): sample moved from development branch

--- src/button/button.module.ts
+++ src/button/button.module.ts
@@ -1,18 +1,22 @@
 import { NgModule, ModuleWithProviders } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { DefaultButtonController } from './default.component';
+import { CheckBoxModule  } from '@syncfusion/ej2-ng-buttons';
+import { CheckBoxController } from './check-box.component';
 import { SharedModule } from '../common/shared.module';
 
 export const buttonAppRoutes: Object[] = [
-    { path: 'button/default', component: DefaultButtonController, name: 'Default Functionalities', category: 'Button' }
+    { path: 'button/default', component: DefaultButtonController, name: 'Default Functionalities', category: 'Button' },
+    { path: 'button/check-box', component: CheckBoxController, name: 'CheckBox', category: 'Button', type: 'new' }
 ];
 
 export const buttonRouter: ModuleWithProviders = RouterModule.forChild(buttonAppRoutes);
 
 @NgModule({
-    imports: [buttonRouter, SharedModule],
+    imports: [buttonRouter, CheckBoxModule, SharedModule],
     declarations: [
-        DefaultButtonController
+        DefaultButtonController,
+        CheckBoxController
     ]
 })
 export class ButtonModule { }
---
+++ src/button/check-box.component.ts
@@ -0,0 +1,25 @@
+import { Component, ViewEncapsulation, Inject, ViewChild } from '@angular/core';
+import { CheckBoxComponent } from '@syncfusion/ej2-ng-buttons';
+
+/**
+ * CheckBox Controller
+ */
+@Component({
+    selector: 'control-content',
+    templateUrl: 'check-box.html',
+    styleUrls: ['check-box.css'],
+    encapsulation: ViewEncapsulation.None
+})
+
+export class CheckBoxController {
+    @ViewChild('checkbox')
+    public checkbox: CheckBoxComponent;
+
+    constructor(@Inject('sourceFiles') private sourceFiles: any) {
+         sourceFiles.files = ['check-box.css'];
+    }
+
+    public changeHandler() : void {
+        this.checkbox.label = 'CheckBox: ' + this.checkbox.checked;
+    }
+ }
