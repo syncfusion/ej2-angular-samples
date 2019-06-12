dbca5c584bb11e08c6b88781028cac7296823617
Your Name <you@example.com>
2019-06-12 19:47:12 -0400

    compilation issue

--- src/app/sparkline/sparkline.module.ts
+++ src/app/sparkline/sparkline.module.ts
@@ -9,7 +9,6 @@ import { DefaultSparklineComponent } from './default.component';
 import { AxisTypeSparklineComponent } from './axis-type.component';
 import { SeriesTypeSparklineComponent } from './series-type.component';
 import { SparklineCustomizationSample } from './customization.component';
-import { SparkineLiveUpdateSample } from './live-update.component';
 import { SparklineGridSample } from './sparkline-grid.component';
 import { SparklineRangebandSample } from './range-band.component';
 import { SliderModule } from '@syncfusion/ej2-angular-inputs';
@@ -26,13 +25,12 @@ export const sparklineAppRoutes: Object[] = [
      order: '01', category: 'Sparkline Charts', description: 'This demo for Essential JS2 Sparkline control demonstrates rendering sparklines in the data grid control.' },
     { path: ':theme/sparkline/customization', component: SparklineCustomizationSample, name: 'Customization', type: 'update',
      order: '01', category: 'Sparkline Charts', description: 'This demo for Essential JS2 Sparkline control depicts the properties required to customize the sparkline.' },
-    { path: ':theme/sparkline/live-update', component: SparkineLiveUpdateSample, name: 'Live Update', order: '01', category: 'Sparkline Charts', description: 'This demo for Essential JS2 Sparkline control depicts the task manager with live data update for sparklines.' },
     { path: ':theme/sparkline/range-band', component: SparklineRangebandSample, name: 'Range Band', order: '01', category: 'Sparkline Charts', description: 'This demo for Essential JS2 Sparkline control depicts the range band feature and its customization options.' }
 ];
 
 export const sparklineRouter: ModuleWithProviders = RouterModule.forChild(sparklineAppRoutes);
 let declarations: Type<Object>[] = [DefaultSparklineComponent, AxisTypeSparklineComponent, SeriesTypeSparklineComponent,
-    SparklineGridSample, SparklineRangebandSample, SparklineCustomizationSample, SparkineLiveUpdateSample];
+    SparklineGridSample, SparklineRangebandSample, SparklineCustomizationSample];
 @NgModule({
     imports: [sparklineRouter, SparklineAllModule, GridAllModule, SliderModule, DropDownListAllModule, CheckBoxModule],
     exports: [],
